dc61e2477decd7008278ccb1139e54d5f2064a7f
eve <eve@difi.no>
2018-12-06 12:38:31 +0100

    PBLEID-17124 First commit

---
+++ src/main/java/no/difi/oauth2/utils/jwtgrantperformance/JwtGrantPerformanceApplication.java
@@ -0,0 +1,12 @@
+package no.difi.oauth2.utils.jwtgrantperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtGrantPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtGrantPerformanceApplication.class, args);
+    }
+}
---
+++ src/test/java/no/difi/oauth2/utils/jwtgrantperformance/JwtGrantPerformanceApplicationTests.java
@@ -0,0 +1,16 @@
+package no.difi.oauth2.utils.jwtgrantperformance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtGrantPerformanceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
